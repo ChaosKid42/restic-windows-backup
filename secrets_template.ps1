28e2f572565758588046eb4494d1db6ef802e65d
Christoph Scholz <christoph.scholz@gmail.com>
2023-01-15 12:14:23 +0100

    Switch from AWS envvars to B2

--- secrets_template.ps1
+++ secrets_template.ps1
@@ -3,8 +3,8 @@
 # Rename to `secrets.ps1`
 
 # restic backup repository configuration
-$Env:AWS_ACCESS_KEY_ID='<KEY>'
-$Env:AWS_SECRET_ACCESS_KEY='<KEY>'
+$Env:B2_ACCOUNT_ID='<KEY>'
+$Env:B2_ACCOUNT_KEY='<KEY>'
 $Env:RESTIC_REPOSITORY='<REPO URL>'
 $Env:RESTIC_PASSWORD='<BACKUP PASSWORD>'
 
